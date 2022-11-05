efaa9b05dc685f88c042ca19724aeafe510e468f
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-05 15:37:18 +0300

    05-11-22

--- XmlDocumentProject/Program.cs
+++ XmlDocumentProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml;
+using System.Xml.Linq;
 
 namespace XmlDocumentProject
 {
@@ -71,18 +72,84 @@ namespace XmlDocumentProject
 
 
 
-            // Open Xml Document? read and remove Eleement
+            //Open Xml Document? read and remove Eleement
 
             //XmlDocument document = new XmlDocument();
             //document.Load("users.xml");
 
-            //XmlNode? node = document.SelectSingleNode("//City");
-            //Console.WriteLine(node?.InnerText);
-            //XmlNode? nodeParent = node?.ParentNode;
-            //Console.WriteLine(nodeParent?.Name);
+            //var nodes = document.DocumentElement.SelectNodes("//*[@Type=\"Last\"]");
+            ////Console.WriteLine(node?.InnerText);
+            ////XmlNode? nodeParent = node?.ParentNode;
+            //foreach(XmlNode item in nodes)
+            //{
+            //    //var attr = item.SelectNodes("@+");
+            //    //Console.WriteLine(attr);
+            //    Console.WriteLine(item.Name);
+            //    Console.WriteLine(item.InnerText);
+            //    Console.WriteLine(item.InnerXml);
+            //}
+
             //nodeParent?.RemoveChild(node!);
 
             //document.Save("users.xml");
+
+
+            //XDocument xDocument = new();
+            //xDocument.Add(new XElement("Users"));
+            //XElement xUser = new(nameof(User));
+            //XElement xName = new(nameof(User.Name), user.Name);
+            //XAttribute xType = new("Type", "First");
+            //xName.Add(xType);
+            //xUser.Add(xName);
+            //xUser.Add(new XElement(nameof(User.Age), user.Age));
+
+            //xDocument.Root.Add(xUser);
+
+
+            //XDocument xDocument = new XDocument(
+            //                         new XElement("Users",
+            //                            new XElement(nameof(User),
+            //                                new XElement(nameof(User.Name), user.Name, 
+            //                                    new XAttribute("Type", "First")
+            //                                ),
+            //                                new XElement(nameof(User.Age), user.Age),
+            //                                new XElement(nameof(Address),
+            //                                    new XElement(nameof(Address.City), user.Address.City),
+            //                                    new XElement(nameof(Address.Street), user.Address.Street)
+            //                                )
+            //                            )
+            //                         )
+            //                      );
+            //xDocument.Save("usersLinq.xml");
+
+
+            XDocument document = XDocument.Load("users.xml");
+            XElement? root = document.Root;
+
+            //foreach(XElement element in root.Elements("User"))
+            //{
+            //    Console.WriteLine($"Name: {element.Element("Name").Value}");
+            //    Console.WriteLine($"Age: {element.Element("Age").Value}");
+            //    Console.WriteLine();
+            //}
+
+            var users = document.Element("Users")
+                                .Elements("User")
+                                .Where(u => u.Element("Address").Element("City")?.Value == "Moscow")
+                                .Select(u => new
+                                {
+                                    Name = u.Element("Name")?.Value,
+                                    Type = u.Element("Name").Attribute("Type")?.Value,
+                                    Age = u.Element("Age")?.Value,
+                                    City = u.Element("Address").Element("City")?.Value,
+                                    Street = u.Element("Address").Element("Street")?.Value
+                                });
+
+            foreach(var u in users)
+            {
+                Console.WriteLine($"[{u.Type}] {u.Name} ({u.Age}) -> {u.City} / {u.Street}");
+            }
+
         }
     }
 }
